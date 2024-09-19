9f302c90a26f9dbe140b3033cbbffe84f69825aa
le2xx <le2x@rambler.ru>
2024-09-19 13:52:48 +0300

    fix theme

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, signal } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { tap } from 'rxjs';
 import { ThemeService } from './services/theme.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,14 @@ export class AppComponent {
   themes = signal<string[]>(['light', 'dark']);
   formControl = new FormControl<string>('light');
 
-  constructor(private themeService: ThemeService) {
+  constructor(
+    private themeService: ThemeService,
+    private destroyRef: DestroyRef
+  ) {
     this.formControl.valueChanges
       .pipe(
-        tap((v) => {
-          console.log(v);
-          this.themeService.setTheme(v as string);
-        })
+        tap((v) => this.themeService.setTheme(v as string)),
+        takeUntilDestroyed(this.destroyRef)
       )
       .subscribe();
   }
